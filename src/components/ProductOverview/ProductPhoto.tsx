f819585719d411a6da3faf15ee9436fc91125b88
Suraj Kumar <49224173+surajkumar85@users.noreply.github.com>
2023-11-20 21:40:03 +0530

    Update ProductPhoto.tsx

--- src/components/ProductOverview/ProductPhoto.tsx
+++ src/components/ProductOverview/ProductPhoto.tsx
@@ -1,7 +1,7 @@
 export default function ProductPhoto() {
   return (
     <div className="w-full flex items-center justify-center h-[400px] bg-white rounded-lg relative sm-3">
-      <img src="/assets/product.png" alt="product" />
+      <img src="/assets/product.png" alt="product" className="w-[50%]" />
       <img
         src="/assets/veg.png"
         alt="veg symbol"
