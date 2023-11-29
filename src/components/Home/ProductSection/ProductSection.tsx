e4a650e13c0018ae89762bff20a38f33d26479cd
Suraj Kumar <49224173+surajkumar85@users.noreply.github.com>
2023-11-29 23:46:18 +0530

    added hom products

---
+++ src/components/Home/ProductSection/ProductSection.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { ProductCard, SectionHeader, SliderWrapper } from "@/components";
+import { SwiperSlide } from "swiper/react";
+import { useAppContext } from "@/provider/ContextProvider/ContextProvider";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  products: any[];
+  sectionName: string;
+};
+
+export default function ProductSection({ products, sectionName }: Props) {
+  const { slidePerView } = useAppContext();
+  const router = useRouter();
+  return (
+    <>
+      {products?.length > 0 && (
+        <div className="w-[1200px] mx-auto mt-[60px] max-[1200px]:w-full flex flex-col gap-[40px] px-5">
+          <SectionHeader
+            onClick={() => router.push("/shop-by-brands")}
+            label={sectionName}
+            showBtn
+            btnLabel="View all"
+          />
+          <SliderWrapper showBtns slidePerView={slidePerView}>
+            {products.map((item, index) => (
+              <SwiperSlide>
+                <ProductCard />
+              </SwiperSlide>
+            ))}
+          </SliderWrapper>
+        </div>
+      )}
+    </>
+  );
+}
